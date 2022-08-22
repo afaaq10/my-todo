20f98eca66e695637e0277e339e625b728ba9131
Afaaq <afaaqmajeed123@gmail.com>
2022-08-22 16:44:23 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import { useState, setTimeout } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Todos from './Todos';
 import Button from '@mui/material/Button';
-import knockSound from './knockSound.mp3'
+import knockSound from './knockSound.mp3';
+import 'animate.css';
+
+
+
 
 
 function App() {
+
+  setTimeout(() => {
+
+    document.getElementById('h1').innerHTML = " "
+
+  }, 2000);
+
+
+
+
 
 
   const [input, setInput] = useState("")
@@ -36,6 +50,22 @@ function App() {
     setTodo([])
 
   }
+  const handleEnter = (e) => {
+    if (e.key == "Enter") {
+      if (!input) {
+        alert("Please add a note first")
+      }
+      else {
+        setTodo([...todo, { id: Date.now(), text: input }])
+        setInput("")
+      }
+      const audio = new Audio(knockSound);
+      audio.play()
+
+
+    }
+
+  }
 
 
 
@@ -43,10 +73,13 @@ function App() {
 
   return (
     <div className="App">
+      <h1 id="h1" className="animate__animated  animate__backInUp" style={{ textAlign: "center", color: "pink" }}>Hey! Welcome</h1>
       <img style={{ width: "110px", display: "flex", margin: "auto" }} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCEK3wdUPsd4E2REc2j9G7A7CIUEE6FhjtHQ&usqp=CAU" alt="image" />
 
+
+
       <div className="App__inbut">
-        <input type="text" placeholder='Add your new task' onChange={handleInput} value={input} />
+        <input className='input1' type="text" placeholder='Add your new task' onChange={handleInput} value={input} onKeyDown={handleEnter} />
 
         <div className="add"><Button onClick={handleAdd} variant="contained">Add</Button></div>
       </div>
--- src/Todos.js
+++ src/Todos.js
@@ -4,7 +4,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import knockSound from './knockSound.mp3'
 import { useState } from 'react';
-const Todos = ({ newTodo, todo, id, setTodo, input }) => {
+const Todos = ({ newTodo, todo, id, setTodo }) => {
 
 
     const [first, setfirst] = useState(false)
@@ -18,7 +18,10 @@ const Todos = ({ newTodo, todo, id, setTodo, input }) => {
     const handleDelete = (idR) => {
         const audio = new Audio(knockSound);
         audio.play()
+
         setTodo(todo.filter((x) => x.id != idR))
+        alert("Note deleted successfully")
+
     }
 
     const handleEdit = () => {
@@ -28,11 +31,16 @@ const Todos = ({ newTodo, todo, id, setTodo, input }) => {
             setfirst(!first)
         else {
             setfirst(!first)
-            setTodo(todo.map((x) => x.id == todo.id ? {
+            setTodo(todo.map((x) => x.id
+                == id ? {
+
                 id: x.id,
                 text: input2
             } : x))
+            alert("Note edited successfully")
+
         }
+
 
         const audio = new Audio(knockSound);
         audio.play()
@@ -42,15 +50,15 @@ const Todos = ({ newTodo, todo, id, setTodo, input }) => {
     return (
         <div className='todos'>
 
+            <div className="todos__wrap">
+                {!first ? <div className="text"><p> {newTodo}</p></div> : <input className='edit_input' type="text" value={input2} onChange={handleChange}
 
-            {!first ? <div className="text"> {newTodo}</div> : <input className='edit_input' type="text" value={input2} onChange={handleChange}
-
-            />}
-
+                />}
+            </div>
             <div className="todos__edit">
 
                 <div className="edit"><EditIcon onClick={handleEdit} /></div>
-                <div> <DeleteIcon onClick={() => handleDelete(id)} /></div>
+                <div className="edit2"> <DeleteIcon onClick={() => handleDelete(id)} /></div>
 
             </div>
 
